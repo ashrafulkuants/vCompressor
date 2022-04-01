341e49358101cf46f504945c1a6dab377db24ae0
coderashraf <ashrafulbnt@gmail.com>
2022-04-01 15:45:02 +0530

    second

--- videoCompressor.js
+++ videoCompressor.js
@@ -1,6 +1,6 @@
 const {exec} = require('child_process');
-const temp= new Date().valueOf();
-exec(`ffmpeg -n -loglevel error -i input.mp4 -vcodec libx264 -crf 30 -preset faster -tune film ${temp}.mp4`,(error,stdout,stderr)=>{
+// const temp= new Date().valueOf();
+exec(`ffmpeg -n -loglevel error -i input.mp4 -vcodec libx264 -crf 30 -preset faster -tune film output.mp4`,(error,stdout,stderr)=>{
   if(error){
     console.log(`error:${error.message}`);
     return;
